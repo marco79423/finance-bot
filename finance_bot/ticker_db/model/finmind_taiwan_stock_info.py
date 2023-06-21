49475eb841a82ffa78fd9315b4c68260fbed9442
兩大類 <marco79423@gmail.com>
2023-06-21 08:37:48 +0800

    feat: 修正 finmind 的 model 和 finmind_updater.py 的寫法

--- finance_bot/ticker_db/model/finmind_taiwan_stock_info.py
+++ finance_bot/ticker_db/model/finmind_taiwan_stock_info.py
@@ -6,15 +6,15 @@ from .base import Base
 class FinmindTaiwanStockInfo(Base):
     __tablename__ = 'finmind_taiwan_stock_info'
     __table_args__ = (
-        Index('industry_category'),
-        Index('type'),
+        Index('idx_industry_category', 'industry_category'),
+        Index('idx_type', 'type'),
     )
 
     stock_id = Column(String, nullable=False, primary_key=True)
     industry_category = Column(String, nullable=False, default='')
     stock_name = Column(String, nullable=False, default='')
     type = Column(String, nullable=False, default='')
-    date = Column(String, nullable=False, default='')
+    date = Column(String, nullable=False)
 
     created_at = Column(DateTime, nullable=False, server_default=func.now())
     updated_at = Column(DateTime, nullable=False, server_default=func.now(), onupdate=func.now())
--- finance_bot/ticker_db/updater/finmind_updater.py
+++ finance_bot/ticker_db/updater/finmind_updater.py
@@ -6,7 +6,7 @@ from finance_bot.config import conf
 from finance_bot.ticker_db.model import FinmindTaiwanStockInfo
 
 
-def get_finmind_loader():
+def get_finmind_data_loader():
     api = DataLoader()
     api.login_by_token(api_token=conf.ticker_db.updater.finmind.api_token)
     return api
@@ -14,15 +14,13 @@ def get_finmind_loader():
 
 class FinmindUpdater:
     def __init__(self):
-        self.api = get_finmind_loader()
+        self.data_loader = get_finmind_data_loader()
 
     def update_taiwan_stock_info(self, session):
-        df = self.api.taiwan_stock_info()
-
-        with Session(engine) as session:
-            for _, data in df.iterrows():
-                self._save_or_update_taiwan_stock_info(session, data)
-            session.commit()
+        df = self.data_loader.taiwan_stock_info()
+        for _, data in df.iterrows():
+            self._save_or_update_taiwan_stock_info(session, data)
+        session.commit()
 
     @staticmethod
     def _save_or_update_taiwan_stock_info(session, data):
