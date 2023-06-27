dc4fb44b1271745eff466dfd0224d20afa6f4af7
兩大類 <marco79423@gmail.com>
2023-06-27 23:05:27 +0800

    fix: 修正快取的 bug

--- finance_bot/ticker_db/ticker_db.py
+++ finance_bot/ticker_db/ticker_db.py
@@ -14,114 +14,119 @@ class Ticker:
 
     def get_close_prices(self) -> pd.Series:
         """取得收盤價"""
-        if self._metric_cache.get('close') is None:
+        cache_key = 'close'
+        s = self._metric_cache.get(cache_key)
+        if s is None:
             df = pd.read_sql(
-                sql=text("SELECT price, date FROM finlab_price_close WHERE symbol=:symbol"),
+                sql=text("SELECT price AS close, date FROM finlab_price_close WHERE symbol=:symbol"),
                 params={'symbol': self.symbol},
                 con=self.ticker_db.engine,
                 index_col='date',
                 parse_dates=['date'],
             )
-            self._metric_cache['close'] = df['price']
-        return self._metric_cache['close']
+            s = self._metric_cache[cache_key] = df['close']
+        return s.copy()
 
     def get_share_capitals(self, to_date=False) -> pd.Series:
         """取得股本"""
-        if self._metric_cache.get('share_capitals') is None:
+        cache_key = f'share_capitals:{to_date}'
+        s = self._metric_cache.get(cache_key)
+        if s is None:
             df = pd.read_sql(
-                sql=text("SELECT value, date FROM finlab_share_capital WHERE symbol=:symbol"),
+                sql=text("SELECT value AS share_capitals, date FROM finlab_share_capital WHERE symbol=:symbol"),
                 params={'symbol': self.symbol},
                 con=self.ticker_db.engine,
                 index_col='date',
             )
-
             df.index = pd.PeriodIndex(df.index, freq='Q')
             if to_date:
                 df.index = df.index.to_timestamp()
-
-            self._metric_cache['share_capitals'] = df['value']
-        return self._metric_cache['share_capitals']
+            s = self._metric_cache[cache_key] = df['share_capitals']
+        return s.copy()
 
     def get_market_capitalization(self) -> pd.Series:
         """取得市值"""
-        if self._metric_cache.get('market_capitalization') is None:
+        cache_key = 'market_capitalization'
+        s = self._metric_cache.get(cache_key)
+        if s is None:
             close = self.get_close_prices()
-            share_capitals = self.get_share_capitals()
+            share_capitals = self.get_share_capitals(to_date=True)
             df = pd.DataFrame({
                 'close': close,
                 'share_capitals': share_capitals,
             })
             df = df.fillna(method='ffill')
-            self._metric_cache['market_capitalization'] = df['close'] * df['share_capitals'] / 10
-        return self._metric_cache['market_capitalization']
+            df['market_capitalization'] = df['close'] * df['share_capitals'] / 10
+            s = self._metric_cache[cache_key] = df['market_capitalization']
+        return s.copy()
 
     def get_free_cash_flow(self, to_date=False) -> pd.Series:
         """取得自由現金流"""
-        if self._metric_cache.get('free_cash_flow') is None:
+        cache_key = f'free_cash_flow:{to_date}'
+        s = self._metric_cache.get(cache_key)
+        if s is None:
             df = pd.read_sql(
-                sql=text("SELECT value, date FROM finlab_free_cash_flow WHERE symbol=:symbol"),
+                sql=text("SELECT value AS free_cash_flow, date FROM finlab_free_cash_flow WHERE symbol=:symbol"),
                 params={'symbol': self.symbol},
                 con=self.ticker_db.engine,
                 index_col='date',
             )
-
             df.index = pd.PeriodIndex(df.index, freq='Q')
             if to_date:
                 df.index = df.index.to_timestamp()
-
-            self._metric_cache['free_cash_flow'] = df['value']
-        return self._metric_cache['free_cash_flow']
+            s = self._metric_cache[cache_key] = df['free_cash_flow']
+        return s.copy()
 
     def get_earning_per_share(self, to_date=False) -> pd.Series:
         """取得每股稅後淨利(EPS)"""
-        if self._metric_cache.get('earning_per_share') is None:
+        cache_key = f'earning_per_share:{to_date}'
+        s = self._metric_cache.get(cache_key)
+        if s is None:
             df = pd.read_sql(
-                sql=text("SELECT value, date FROM finlab_earning_per_share WHERE symbol=:symbol"),
+                sql=text("SELECT value AS earning_per_share, date FROM finlab_earning_per_share WHERE symbol=:symbol"),
                 params={'symbol': self.symbol},
                 con=self.ticker_db.engine,
                 index_col='date',
             )
-
             df.index = pd.PeriodIndex(df.index, freq='Q')
             if to_date:
                 df.index = df.index.to_timestamp()
-
-            self._metric_cache['earning_per_share'] = df['value']
-        return self._metric_cache['earning_per_share']
+            s = self._metric_cache[cache_key] = df['earning_per_share']
+        return s.copy()
 
     def get_return_on_equity(self, to_date=False) -> pd.Series:
         """取得股東權益報酬率(ROE)"""
-        if self._metric_cache.get('return_on_equity') is None:
+        cache_key = f'return_on_equity:{to_date}'
+        s = self._metric_cache.get(cache_key)
+        if s is None:
             df = pd.read_sql(
-                sql=text("SELECT value, date FROM finlab_return_on_equity WHERE symbol=:symbol"),
+                sql=text("SELECT value AS return_on_equity, date FROM finlab_return_on_equity WHERE symbol=:symbol"),
                 params={'symbol': self.symbol},
                 con=self.ticker_db.engine,
                 index_col='date',
             )
-
             df.index = pd.PeriodIndex(df.index, freq='Q')
             if to_date:
                 df.index = df.index.to_timestamp()
-
-            self._metric_cache['return_on_equity'] = df['value']
-        return self._metric_cache['return_on_equity']
+            s = self._metric_cache[cache_key] = df['return_on_equity']
+        return s.copy()
 
     def get_operating_income(self, to_date=False) -> pd.Series:
         """取得營業利益"""
-        if self._metric_cache.get('operating_income') is None:
+        cache_key = f'operating_income:{to_date}'
+        s = self._metric_cache.get(cache_key)
+        if s is None:
             df = pd.read_sql(
-                sql=text("SELECT value, date FROM finlab_operating_income WHERE symbol=:symbol"),
+                sql=text("SELECT value AS operating_income, date FROM finlab_operating_income WHERE symbol=:symbol"),
                 params={'symbol': self.symbol},
                 con=self.ticker_db.engine,
                 index_col='date',
             )
-
             df.index = pd.PeriodIndex(df.index, freq='Q')
             if to_date:
                 df.index = df.index.to_timestamp()
-
-            self._metric_cache['operating_income'] = df['value']
-        return self._metric_cache['operating_income']
+            s = self._metric_cache[cache_key] = df['operating_income']
+        return s.copy()
 
     def get_all_metrics(self):
         df = pd.DataFrame({
@@ -179,12 +184,14 @@ class TickerDB:
 if __name__ == '__main__':
     ticker_db = TickerDB()
     # print(ticker_db.get_all_tickers())
-    print(ticker_db.get_all_tickers_with_all_metrics())
+    # print(ticker_db.get_all_tickers_with_all_metrics())
     ticker = ticker_db.get_ticker('1101')
     # print(ticker.get_close_prices())
     # print(ticker.get_share_capitals())
+    # print(ticker.get_market_capitalization())
     # print(ticker.get_free_cash_flow())
     # print(ticker.get_earning_per_share())
     # print(ticker.get_return_on_equity())
     # print(ticker.get_operating_income(to_date=True))
+    print(ticker.get_all_metrics())
     print(ticker.get_all_metrics())
