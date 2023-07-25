95aefb6060228bed35604211f9b94b9eb34feb6a
兩大類 <marco79423@gmail.com>
2023-07-25 08:38:08 +0800

    feat: 新增 /debug/scheduled-jobs API

--- finance_bot/server/router/debug.py
+++ finance_bot/server/router/debug.py
@@ -15,3 +15,9 @@ async def ping():
 async def send_notification():
     bot = telegram.Bot(conf.notification.telegram.token)
     await bot.send_message(chat_id=conf.notification.telegram.chat_id, text='理財機器人通知測試')
+
+
+@router.get('/scheduled-jobs')
+async def get_scheduled_jobs(request: fastapi.Request):
+    jobs = request.app.state.scheduler.get_jobs()
+    return [str(job) for job in jobs]
