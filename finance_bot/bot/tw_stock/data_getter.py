47dcacab5447e7e42173dcb59c6043ce2ef6dfa0
兩大類 <marco79423@gmail.com>
2023-08-06 12:10:32 +0800

    feat: add close to data getter

--- finance_bot/bot/tw_stock/data_getter.py
+++ finance_bot/bot/tw_stock/data_getter.py
@@ -8,10 +8,16 @@ from ...infrastructure import infra
 class DataGetter:
 
     def __init__(self):
+        self._prices_df = None
         self._monthly_revenue_df = None
 
     def __getitem__(self, stock_id):
         return StockGetter(stock_id)
+
+    @property
+    def close(self):
+        df = self._get_prices_df()
+        return df.pivot(columns='stock_id', values='close')
 
     @property
     def monthly_revenue(self):
@@ -24,3 +30,13 @@ class DataGetter:
             df = df.set_index('date')
             self._monthly_revenue_df = df.pivot(columns='stock_id', values='revenue')
         return self._monthly_revenue_df
+
+    def _get_prices_df(self):
+        if self._prices_df is None:
+            self._prices_df = pd.read_sql(
+                sql=text("SELECT * FROM tw_stock_price"),
+                con=infra.db.engine,
+                index_col='date',
+                parse_dates=['date'],
+            )
+        return self._prices_df
