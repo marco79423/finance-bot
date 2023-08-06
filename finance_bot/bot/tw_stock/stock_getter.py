7417631c85c8707dbc97ee4044a3d099a65cf91d
兩大類 <marco79423@gmail.com>
2023-08-06 11:08:30 +0800

    add monthly_revenue to data getter

--- finance_bot/bot/tw_stock/stock_getter.py
+++ finance_bot/bot/tw_stock/stock_getter.py
@@ -9,6 +9,7 @@ class StockGetter:
     def __init__(self, stock_id):
         self.stock_id = stock_id
         self._prices_df = None
+        self._monthly_revenue_df = None
 
     @property
     def open(self):
@@ -19,6 +20,21 @@ class StockGetter:
     def close(self):
         df = self._get_prices_df()
         return df['close']
+
+    @property
+    def monthly_revenue(self):
+        if self._monthly_revenue_df is None:
+            self._monthly_revenue_df = pd.read_sql(
+                sql=text("SELECT * FROM tw_stock_monthly_revenue WHERE stock_id=:stock_id"),
+                params={
+                    'stock_id': str(self.stock_id),  # 確保輸入的是字串
+                },
+                con=infra.db.engine,
+            )
+            self._monthly_revenue_df['date'] = pd.to_datetime(self._monthly_revenue_df['date']).dt.to_period('M')
+            self._monthly_revenue_df.set_index('date')
+
+        return self._monthly_revenue_df
 
     def _get_prices_df(self):
         if self._prices_df is None:
