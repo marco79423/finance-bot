1f39767daff35d382360966099da541a92cd78b3
兩大類 <marco79423@gmail.com>
2023-08-06 11:19:59 +0800

    feat: init env when the shell be started

--- finance_bot/shell/config.py
+++ finance_bot/shell/config.py
@@ -8,12 +8,16 @@ def get_config(url):
         'import os',
         'import numpy as np',
         'import pandas as pd',
+        'from finance_bot.bot.tw_stock import TWStockBot',
 
         # 環境變數
         f'os.environ["FINB_SERVER_URL"]="{url}"',
 
         # 重要設定
         'pd.set_option("plotting.backend", "plotly")',
+
+        # 初始化
+        'tw_stock_bot = TWStockBot()',
     ]
     c.InteractiveShellApp.extensions = [
         'finance_bot.shell.extension'
