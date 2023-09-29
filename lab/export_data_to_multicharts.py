2a694ed269b74cc5375e90a2c409af62f4776ea6
兩大類 <marco79423@gmail.com>
2023-09-29 08:59:25 +0800

    fix: update export_data_to_multicharts.py

--- lab/export_data_to_multicharts.py
+++ lab/export_data_to_multicharts.py
@@ -2,11 +2,13 @@ from finance_bot.core import TWStockManager
 from finance_bot.infrastructure import infra
 
 data = TWStockManager().data
-for stock_id in data.stocks.index:
-    print(f'更新 {stock_id} ...')
-    target_path = infra.path.multicharts_folder / f'{stock_id}.csv'
 
-    prices_df = data[stock_id].prices
-    df = prices_df[['open', 'high', 'low', 'close', 'volume']]
+for stock_id in data.stocks[data.stocks['tracked'] == 1].index:
+    print(f'更新 {stock_id} ...')
+    df = data[stock_id].prices[['open', 'high', 'low', 'close', 'volume']]
+    if df.empty:
+        continue
+
+    target_path = infra.path.multicharts_folder / f'{stock_id}.csv'
     df.to_csv(target_path)
 print(f'更新股價結束')
