1f9dc033873a7ed4d7a3940f5a6dcf06c61fa9f0
兩大類 <marco79423@gmail.com>
2023-10-02 10:23:44 +0800

    fix: update export_data_to_multicharts.py

--- lab/export_data_to_multicharts.py
+++ lab/export_data_to_multicharts.py
@@ -3,11 +3,15 @@ from finance_bot.infrastructure import infra
 
 data = TWStockManager().data
 
-for stock_id in data.stocks[data.stocks['tracked'] == 1].index:
+for stock_id, stock in data.stocks[data.stocks['tracked'] == 1].iterrows():
     print(f'更新 {stock_id} ...')
-    df = data[stock_id].prices[['open', 'high', 'low', 'close', 'volume']]
+    df = data[stock_id].prices[['open', 'high', 'low', 'close', 'volume']].copy()
     if df.empty:
         continue
+
+    df['description'] = f'{stock.name} (Custom)'
+    df['type'] = 'Stock'
+    df['exchange'] = 'TWSE'  # TSEC
 
     target_path = infra.path.multicharts_folder / f'{stock_id}.csv'
     df.to_csv(target_path)
