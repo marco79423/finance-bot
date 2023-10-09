58cd3cd97a27c8009205d0c7bb6f0d0cadf0aa4d
兩大類 <marco79423@gmail.com>
2023-10-09 13:39:29 +0800

    feat: refactor export_data_to_multicharts.py

--- tools/export_data_to_multicharts.py
+++ tools/export_data_to_multicharts.py
@@ -1,15 +1,25 @@
 from finance_bot.core import TWStockManager
 from finance_bot.infrastructure import infra
 
-data = TWStockManager().data
-data.rebuild_cache()
 
-for stock_id in data.stocks[data.stocks['tracked'] == 1].index:
-    print(f'更新 {stock_id} ...')
-    df = data[stock_id].prices[['open', 'high', 'low', 'close', 'volume']]
-    if df.empty:
-        continue
+def export_price_data_to_multicharts(data):
+    for stock_id in data.stocks[data.stocks['tracked'] == 1].index:
+        print(f'更新 {stock_id} ...')
+        df = data[stock_id].prices[['open', 'high', 'low', 'close', 'volume']]
+        if df.empty:
+            continue
 
-    target_path = infra.path.multicharts_folder / 'prices' / f'{stock_id}.csv'
-    df.to_csv(target_path)
-print(f'更新股價結束')
+        target_path = infra.path.multicharts_folder / 'prices' / f'{stock_id}.csv'
+        df.to_csv(target_path)
+    print(f'更新股價結束')
+
+
+def main():
+    data = TWStockManager().data
+    data.rebuild_cache()
+
+    export_price_data_to_multicharts(data)
+
+
+if __name__ == '__main__':
+    main()
