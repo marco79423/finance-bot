e27679b26a5f4e684fd28650b36050398596f08f
兩大類 <marco79423@gmail.com>
2023-10-29 18:16:45 +0800

    fix: 補完該補的資料

--- tool/backtester/model/limit_data.py
+++ tool/backtester/model/limit_data.py
@@ -7,22 +7,28 @@ class LimitMarketData(MarketDataBase):
         self.start_date = start_date
         self.end_date = end_date
 
+        self._open = self.data.open.ffill()
+        self._close = self.data.close.ffill()
+        self._high = self.data.high.ffill()
+        self._low = self.data.low.ffill()
+        self._volume = self.data.volume.ffill()
+
     @property
     def open(self):
-        return self.data.open[self.start_date:self.end_date]
+        return self._open[self.start_date:self.end_date]
 
     @property
     def close(self):
-        return self.data.close[self.start_date:self.end_date]
+        return self._close[self.start_date:self.end_date]
 
     @property
     def high(self):
-        return self.data.high[self.start_date:self.end_date]
+        return self._high[self.start_date:self.end_date]
 
     @property
     def low(self):
-        return self.data.low[self.start_date:self.end_date]
+        return self._low[self.start_date:self.end_date]
 
     @property
     def volume(self):
-        return self.data.volume[self.start_date:self.end_date]
+        return self._volume[self.start_date:self.end_date]
