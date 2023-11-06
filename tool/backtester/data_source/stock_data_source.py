56a80d3ca200f0a1d065133ca4e61703dbae39a3
兩大類 <marco79423@gmail.com>
2023-11-06 08:33:33 +0800

    refactor: 改用 cache 加速

--- tool/backtester/data_source/stock_data_source.py
+++ tool/backtester/data_source/stock_data_source.py
@@ -1,28 +1,34 @@
 import pandas as pd
 from sqlalchemy import text
+import datetime as dt
 
-from finance_bot.core import TWStockManager
 from finance_bot.infrastructure import infra
-from tool.backtester.data_source.base import DataSourceBase, Product
-from tool.backtester.model import LimitMarketData
+from tool.backtester.data_source.base import DataSourceBase
 
 
 class StockDataSource(DataSourceBase):
+    use_cache = True
+
     def __init__(self, all_stock_ids, start, end):
         super().__init__(start, end)
         self._all_stock_ids = all_stock_ids
 
-        self._prices_df = pd.read_sql(
-            sql=text("SELECT * FROM tw_stock_price WHERE date >= :start AND date <= :end AND stock_id IN :stock_ids"),
-            params={
-                'stock_ids': self._all_stock_ids,
-                'start': self.start_time,
-                'end': self.end_time,
-            },
-            con=infra.db.engine,
-            index_col='date',
-            parse_dates=['date'],
-        )
+        if self.use_cache:
+            self._prices_df = infra.db_cache.read(key='tw_stock_price')
+        else:
+            self._prices_df = pd.read_sql(
+                sql=text(
+                    "SELECT * FROM tw_stock_price WHERE date >= :start AND date <= :end AND stock_id IN :stock_ids"
+                ),
+                params={
+                    'stock_ids': self._all_stock_ids,
+                    'start': self.start_time,
+                    'end': self.end_time,
+                },
+                con=infra.db.engine,
+                index_col='date',
+                parse_dates=['date'],
+            )
 
         self._all_open = self._prices_df.pivot(columns='stock_id', values='open').ffill()  # 補完空值的收盤價
         self._all_close = self._prices_df.pivot(columns='stock_id', values='close').ffill()  # 補完空值的收盤價
