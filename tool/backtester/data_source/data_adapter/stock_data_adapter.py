57a3031d2fa5366d7b8420ae60fd21cfabfdbff5
兩大類 <marco79423@gmail.com>
2023-11-21 21:14:42 +0800

    refactor: 調整寫法

--- tool/backtester/data_source/data_adapter/stock_data_adapter.py
+++ tool/backtester/data_source/data_adapter/stock_data_adapter.py
@@ -8,29 +8,24 @@ from finance_bot.infrastructure import infra
 class StockDataAdapter:
 
     def __init__(self, all_stock_ids=None, start=None, end=None):
-        self._all_stock_ids = all_stock_ids
         self._start_time = pd.Timestamp(start) if start else None
         self._end_time = pd.Timestamp(end) if end else None
 
-        df = infra.db_cache.read(key='tw_stock_price')
-        df = df.sort_index()
-        self._prices_df = df.loc[self._start_time:self._end_time]
+        stock_prices_df = infra.db_cache.read(key='tw_stock_price').sort_index()
+        if self._start_time or self._end_time:
+            stock_prices_df = stock_prices_df.loc[self._start_time:self._end_time]
 
-        if self._start_time is None:
-            self._start_time = self._prices_df.index.min()
-        if self._end_time is None:
-            self._end_time = self._prices_df.index.max()
+        self._start_time = self._start_time or stock_prices_df.index.min()
+        self._end_time = self._end_time or stock_prices_df.index.max()
 
-        if self._all_stock_ids is None:
-            self._all_stock_ids = self._prices_df['stock_id'].unique().tolist()
-        else:
-            self._prices_df = self._prices_df[self._prices_df['stock_id'].isin(self._all_stock_ids)]
+        self._all_stock_ids = all_stock_ids or stock_prices_df['stock_id'].unique().tolist()
 
-        self._open = self._prices_df.pivot(columns='stock_id', values='open').ffill()  # 補完空值的收盤價
-        self._close = self._prices_df.pivot(columns='stock_id', values='close').ffill()  # 補完空值的收盤價
-        self._high = self._prices_df.pivot(columns='stock_id', values='high').ffill()  # 補完空值的最高價
-        self._low = self._prices_df.pivot(columns='stock_id', values='low').ffill()  # 補完空值的最低價
-        self._volume = self._prices_df.pivot(columns='stock_id', values='low').ffill()  # 補完空值的最低價
+        stock_prices_df = stock_prices_df[stock_prices_df['stock_id'].isin(self._all_stock_ids)]
+        self._open = stock_prices_df.pivot(columns='stock_id', values='open').ffill()  # 補完空值的收盤價
+        self._close = stock_prices_df.pivot(columns='stock_id', values='close').ffill()  # 補完空值的收盤價
+        self._high = stock_prices_df.pivot(columns='stock_id', values='high').ffill()  # 補完空值的最高價
+        self._low = stock_prices_df.pivot(columns='stock_id', values='low').ffill()  # 補完空值的最低價
+        self._volume = stock_prices_df.pivot(columns='stock_id', values='low').ffill()  # 補完空值的最低價
 
     @property
     def all_stock_ids(self) -> List[str]:
