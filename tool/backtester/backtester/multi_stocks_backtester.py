020fa8bf346f9b198ce4a1d647e50b4709f91165
兩大類 <marco79423@gmail.com>
2023-11-23 08:23:46 +0800

    test: using rich to prettify console

--- tool/backtester/backtester/multi_stocks_backtester.py
+++ tool/backtester/backtester/multi_stocks_backtester.py
@@ -1,6 +1,14 @@
 import datetime as dt
 
 import pandas as pd
+from rich.console import Console
+from rich.progress import (
+    BarColumn,
+    Progress,
+    TextColumn,
+    TimeRemainingColumn,
+    SpinnerColumn,
+)
 
 from tool.backtester.backtester.result import Result
 from tool.backtester.broker import Broker
@@ -30,18 +38,32 @@ class MultiStocksBacktester:
 
         data_source.is_limit = True
 
-        for today in data_source.all_date_range:
-            print(today)
-            data_source.set_time(today)
+        with Progress(
+                SpinnerColumn(),
+                TextColumn("[bold blue]{task.description}", justify="right"),
+                BarColumn(),
+                "{task.completed}/{task.total} [{task.fields[date]}]",
+                "•",
+                "[progress.percentage]{task.percentage:>3.1f}%",
+                "•",
+                TimeRemainingColumn(),
+        ) as progress:
+            task_id = progress.add_task("回測中", date='', start=False)
+            progress.update(task_id, total=len(data_source.all_date_range))
+            progress.start_task(task_id)
+            for today in data_source.all_date_range:
+                progress.update(task_id, advance=1, date=today.strftime('%Y-%m-%d'))
+                data_source.set_time(today)
 
-            for action in strategy.actions:
-                if action['operation'] == 'buy':
-                    broker.buy(stock_id=action['stock_id'], note=action['note'])
-                elif action['operation'] == 'sell':
-                    broker.sell(stock_id=action['stock_id'], note=action['note'])
-            strategy.inter_handle()
+                for action in strategy.actions:
+                    if action['operation'] == 'buy':
+                        broker.buy(stock_id=action['stock_id'], note=action['note'])
+                    elif action['operation'] == 'sell':
+                        broker.sell(stock_id=action['stock_id'], note=action['note'])
+                strategy.inter_handle()
 
-        print('回測花費時間：', dt.datetime.now() - start_time)
+        console = Console()
+        console.print('回測花費時間：', dt.datetime.now() - start_time)
 
         return Result(
             strategy_name=strategy_class.name,
