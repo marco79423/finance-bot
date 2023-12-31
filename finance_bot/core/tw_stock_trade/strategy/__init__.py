dee4714f445ce0d878117e5acf174e8438d47595
兩大類 <marco79423@gmail.com>
2023-12-31 11:47:29 +0800

    feat: 新增 S1V1

--- finance_bot/core/tw_stock_trade/strategy/__init__.py
+++ finance_bot/core/tw_stock_trade/strategy/__init__.py
@@ -1,3 +1,4 @@
 from .base import StrategyBase
 from .strategy_s1v0 import StrategyS1V0
+from .strategy_s1v1 import StrategyS1V1
 from .strategy_new import StrategyNew
---
+++ finance_bot/core/tw_stock_trade/strategy/strategy_s1v1.py
@@ -0,0 +1,150 @@
+import abc
+from types import FunctionType
+
+import pandas as pd
+from sqlalchemy import text
+
+from finance_bot.core.tw_stock_trade.strategy.base import StrategyBase
+from finance_bot.infrastructure import infra
+
+task_stock_tag_df = pd.read_sql(
+    sql=text("SELECT * FROM tw_stock_tag"),
+    con=infra.db.engine,
+)
+df = task_stock_tag_df[task_stock_tag_df['name'] == '自選1']
+available_stock_ids = df['stock_id'].to_list()
+
+
+class SignalBase(abc.ABC):
+
+    def init(self, data):
+        return {}
+
+    @abc.abstractmethod
+    def handle(self, strategy: StrategyBase):
+        pass
+
+
+class BuySignal(SignalBase):
+    available_stock_ids = available_stock_ids
+
+    def init(self, data):
+        return dict(
+            sma10=data.close.rolling(window=10).mean(),
+            sma35=data.close.rolling(window=35).mean(),
+            voc10=(data.volume - data.volume.shift(10)) / data.volume.shift(10) * 100,
+        )
+
+    def handle(self, strategy: StrategyBase):
+        sma10 = strategy.i('sma10')
+        sma35 = strategy.i('sma35')
+        voc10 = strategy.i('voc10')
+
+        cond1 = (strategy.current_shares == 0).reindex(self.available_stock_ids, fill_value=True)
+        cond2 = strategy.close > 10
+        cond3 = (sma10.iloc[-1] > sma35.iloc[-1]) & (sma10.iloc[-2] < sma35.iloc[-2])
+        cond4 = strategy.data.close.iloc[-1] > strategy.data.open.iloc[-1]
+        cond5 = voc10.iloc[-1] < 100
+
+        return (
+            cond1 & cond2 & cond3 & cond4 & cond5,
+            '',
+        )
+
+
+class GoodProfitSellSignal(SignalBase):
+    def __init__(self, ideal_growth_rate=5, accept_loss_rate=2):
+        self.ideal_growth_rate = ideal_growth_rate
+        self.accept_loss_rate = accept_loss_rate
+
+    def handle(self, strategy: StrategyBase):
+        cond1 = strategy.growth_rate * 100 >= self.ideal_growth_rate
+        cond2 = strategy.growth_rate * 100 < (strategy.max_growth_rate * 100 - self.accept_loss_rate)
+
+        return (
+            cond1 & cond2,
+            lambda stock_id: f'{strategy.growth_rate[stock_id] * 100:.2f}%'
+        )
+
+
+class HasProfitSellSignal(SignalBase):
+    def init(self, data):
+        return dict(
+            sma5=data.close.rolling(window=5).mean(),
+            sma35=data.close.rolling(window=35).mean(),
+        )
+
+    def handle(self, strategy: StrategyBase):
+        sma5 = strategy.i('sma5')
+        sma35 = strategy.i('sma35')
+
+        cond1 = strategy.has_profit
+        cond2 = (sma5.iloc[-1] < sma35.iloc[-1]) & (sma5.iloc[-2] > sma35.iloc[-2])
+
+        return (
+            cond1 & cond2,
+            'SMA'
+        )
+
+
+class RunSellSignal(SignalBase):
+    def handle(self, strategy: StrategyBase):
+        cond1 = strategy.has_profit
+        cond2 = strategy.today - strategy.entry_date > pd.Timedelta(days=30 * 2)
+        return (
+            cond1 & cond2,
+            'run'
+        )
+
+
+class SignalStrategyBase(StrategyBase):
+    buy_signals = []
+    sell_signals = []
+
+    def init(self, data):
+        result = {}
+        for signal in [*self.buy_signals, *self.sell_signals]:
+            result = {
+                **result,
+                **signal.init(data),
+            }
+        return result
+
+    # noinspection PyTypeChecker
+    def handle(self):
+        for buy_signal in self.buy_signals:
+            buy_cond, reason = buy_signal.handle(self)
+            target_list = self.new_target_list([
+                buy_cond
+            ])
+
+            for stock_id in target_list:
+                if isinstance(reason, FunctionType):
+                    reason = reason(stock_id)
+                self.buy_next_day_market(stock_id, reason)
+
+        for sell_signal in self.sell_signals:
+            sell_cond, reason = sell_signal.handle(self)
+            target_list = self.new_target_list([sell_cond])
+
+            for stock_id in target_list:
+                if isinstance(reason, FunctionType):
+                    reason = reason(stock_id)
+                self.sell_next_day_market(stock_id, note=reason)
+
+
+class StrategyS1V1(SignalStrategyBase):
+    name = '策略 S1V1'
+    params = dict(
+        max_single_position_exposure=0.1,
+    )
+
+    buy_signals = [
+        BuySignal()
+    ]
+
+    sell_signals = [
+        GoodProfitSellSignal(ideal_growth_rate=5, accept_loss_rate=2),
+        HasProfitSellSignal(),
+        RunSellSignal(),
+    ]
