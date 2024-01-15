7d22d6188906c1aa336d0270571f9bea46cb2a7d
兩大類 <marco79423@gmail.com>
2024-01-15 08:33:56 +0800

    refactor: 新增個股判斷條件

--- tool/export_stock_list_to_tw_stock_tag.py
+++ tool/export_stock_list_to_tw_stock_tag.py
@@ -1,5 +1,6 @@
 import asyncio
 import pandas as pd
+from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from finance_bot.core import DataSync
@@ -25,13 +26,29 @@ async def export_stock_list_to_tw_stock_tag(data):
         await infra.db.batch_insert_or_update(session, TWStockTag, df)
 
 
+async def export_stock_list_to_tw_stock_tag2(data):
+    df = pd.read_sql(
+        sql=text("SELECT DISTINCT stock_id FROM tw_stock_financial_statements"),
+        con=infra.db.engine,
+    )
+
+    df = pd.DataFrame({
+        'stock_id': df['stock_id'],
+        'name': '個股',
+        'reason': '透過月財報判斷個股'
+    })
+
+    async with AsyncSession(infra.db.async_engine) as session:
+        await infra.db.batch_insert_or_update(session, TWStockTag, df)
+
+
 async def main():
     data = DataSync().data
 
-    await export_stock_list_to_tw_stock_tag(data)
+    # await export_stock_list_to_tw_stock_tag(data)
+    await export_stock_list_to_tw_stock_tag2(data)
 
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
-
