2550000814d33fc36c99b23edd858661c5b2d4aa
兩大類 <marco79423@gmail.com>
2024-09-02 10:30:26 +0800

    feat: add notifier retry

--- finance_bot/infrastructure/manager/notifier_manager.py
+++ finance_bot/infrastructure/manager/notifier_manager.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import telegram
 
 from finance_bot.infrastructure.manager.base import ManagerBase
@@ -10,9 +12,15 @@ class NotifierManager(ManagerBase):
         self._telegram_bot = telegram.Bot(self.conf.infrastructure.notification.telegram.token)
 
     async def send(self, message: str):
-        await self._telegram_bot.send_message(
-            chat_id=self.conf.infrastructure.notification.telegram.chat_id,
-            text=message,
-            connect_timeout=60,
-            pool_timeout=5 * 60,
-        )
+        while True:
+            try:
+                await self._telegram_bot.send_message(
+                    chat_id=self.conf.infrastructure.notification.telegram.chat_id,
+                    text=message,
+                    connect_timeout=60,
+                    pool_timeout=5 * 60,
+                )
+                return
+            except telegram.error.RetryAfter as e:
+                await asyncio.sleep(e.retry_after + 1)
+                continue
